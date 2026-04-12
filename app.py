92a244a6998e167aad5b7d0bfa14863a1f6e3f6d
SpottyTheTurtle <69260662+SpottyTheTurtle@users.noreply.github.com>
2026-04-12 16:08:30 +0100

    test data input with flask

--- app.py
+++ app.py
@@ -2,5 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def run_script():
     return 'Hello, World!'
+
+    data = request.get_json()
+
+    name = data.get("name")
+    age = data.get("age")
+
+    result = process_inputs(name, age)
+
+    return jsonify({"result": result})
